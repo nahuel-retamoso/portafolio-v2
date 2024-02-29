39d3e6828e7b5915c91dcc38a634397d3d94b809
nahuel-retamoso <nahuel.retamoso@gmail.com>
2024-02-28 21:18:13 -0300

    footer-color-spacing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer () {
     return(
-        <div class="flex flex-col h-fit px-7 pb-14 pt-14 justify-center align-center lg:ml-6">
-        <p class="text-sm font-extralight text-white/35">Este sitio es el portafolio de Nahuel Retamoso. Hecho con React ,Tailwind, React Spring y React Three Fiber, deploy en Vercel.
+        <div class="flex flex-col h-fit px-7 pb-14 pt-24 justify-center align-center lg:ml-6">
+        <p class="text-sm font-extralight text-white/25">Este sitio es el portafolio de Nahuel Retamoso. Hecho con React ,Tailwind, React Spring y React Three Fiber, deploy en Vercel.
         </p>
-        <p class="text-sm font-extralight text-white/35">
+        <p class="text-sm font-extralight text-white/25">
         This work is based on "keyboard" (https://sketchfab.com/3d-models/keyboard-ba869e8681974cf088736173b8b86fef) by 45P3R4 (https://sketchfab.com/45P3R4) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
         </p>
       </div>
